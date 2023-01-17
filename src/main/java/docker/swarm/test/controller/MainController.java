febcdc8227529183507ff3a9f61f915748ef8e45
조정현 <76645095+jojaeng2@users.noreply.github.com>
2023-01-17 10:47:05 +0900

    Update MainController.java

--- src/main/java/docker/swarm/test/controller/MainController.java
+++ src/main/java/docker/swarm/test/controller/MainController.java
@@ -3,6 +3,7 @@ package docker.swarm.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 public class MainController {
@@ -10,6 +11,7 @@ public class MainController {
 
   @GetMapping("/hello")
   public String getRequest() {
+    System.out.println("Hello");
     return "Hello world!";
   }
 }
